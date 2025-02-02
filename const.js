638ea4f152ed1edf6ea2ebaf639544d81e9c2747
Edward Shao <edwardshao@csie.io>
2025-02-02 19:28:29 +0800

    misc: sheet format change
    remove row "加入時間"

--- const.js
+++ const.js
@@ -2,5 +2,5 @@ const PROJECT_NAME = '惜食惜福';
 const EXPIRED_AFTER_30_DAYS = 30;
 const EXPIRED_AFTER_60_DAYS = 60;
 const ZERO_WASTE_SPREADSHEET_FILENAME = PROJECT_NAME;
-const EXPECTED_SHEET_FORMAT = ["品項", "加入時間", "過期時間", "數量", "位置", "備註", "Owner"];
+const EXPECTED_SHEET_FORMAT = ["品項", "過期時間", "數量", "位置", "備註", "Owner"];
 const REPORT_RECIPIENTS_LIST = ["laface.tw@gmail.com", "joey.ku@gmail.com"];
